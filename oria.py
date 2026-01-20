926c7722cfddfdb738749cdb90e6bb78904069b2
Ori25ane <oriane.arnaud@etu.minesparis.psl.eu>
2026-01-20 15:27:15 +0100

    5

--- oria.py
+++ oria.py
@@ -8,7 +8,7 @@ blanc, sa pos, en vie ou pas
 '''
 
 
-
+import numpy as np
 
 board = [[[]for i in range 8]for j in range 8]
 
@@ -22,13 +22,24 @@ def rev(L):
         L2.append(-e[0],e[1])
     return L2
 
-def verif (piece,pos,coup):
+def deplac (piece,pos,coup):
     if not((np.array([pos]) + np.array([coup]))[0] >= 0 and (np.array([pos]) + np.array([coup]))[0] <= 7 and (np.array([pos]) + np.array([coup]))[1] <=7 and (np.array([pos]) + np.array([coup]))[0]>=0) :
         return False
-    if piece == c :
-        return True
-    if coup[0] > coup[1] : 
-        max = 
+    if piece == c or piece == r:
+        if not (np.array([pos]) + np.array([coup])) in cases_prises :
+            return True
+        else :
+            if cases_prises[(np.array([pos]) + np.array([coup]))]
+            
+            
+    max = 1
+    if coup[0] > coup[1]: 
+        max = 0
+    pos2 = pos.copy()
+    pos2.append
+    while np.array(pos2[max]) != (np.array([pos]) + np.array([coup]))[max]:
+        
+    
     
 
 #coup (piece,pos)
--- oriane.py
+++ oriane.py
@@ -1,1 +1,5 @@
-np.array([])
+L = [0]
+L2 = L.copy()
+L.append(1)
+print(L2)
+print(L)
