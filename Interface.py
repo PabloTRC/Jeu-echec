a34f984904b33fc6a6b0cfacad9267f382c35be4
Pauline Normant <pauline.normant@etu.minesparis.psl.eu>
2026-01-20 14:39:06 +0100

    Création de l'Interface

--- Interface.py
+++ Interface.py
@@ -2,6 +2,8 @@ import pyxel
 
 WIDTH = 30
 HEIGHT = 30
+BLACK = 0
+WHITE = 7
 
 def checkers():
     checkers = set()
@@ -11,6 +13,20 @@ def checkers():
                 checkers.add((x,y))
     return checkers
 
+def display(color, pixels=None):
+    if pixels is None:
+        pyxel.cls(color)
+    elif len(pixels) >= 1 and type(pixels[0]) == int:
+        display(color, [pixels])
+    else:
+        for x, y in pixels:
+            pyxel.pset(x, y, color)
+
+def draw_maze(maze):
+    display(BLACK)
+    display(WHITE, maze)
+
 pyxel.init(WIDTH, HEIGHT)
 draw_maze(checkers())
 pyxel.show()
+
