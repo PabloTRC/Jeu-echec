e199fd5f1a2e67236d2403bd86928f1f06725882
Ori25ane <oriane.arnaud@etu.minesparis.psl.eu>
2026-01-27 16:28:22 +0100

    en ehcec dans possibles

--- oriane.py
+++ oriane.py
@@ -1,55 +1,66 @@
 #[0 si non occupé/ 1 si occupé, "nom de la pièce", 0 si noir 1 si blanc 3 si pas occupé, 0 pour le nombre de fois utilisé]
-import numpy as np
 
-moves = {"r":[(1,0),(1,1)], "d" : [(1,0),(1,1)], "f" : [], "t" : [[1,0]], "c" : [[2,1],[1,2]]}
-from Pablo import Chessboard
+
+moves = {"r":[(1,0),(1,1)], "d" : [(1,0),(1,1)], "f" : [], "t" : [(1,0)], "c" : [(2,1),(1,2)]}
 
 def rev(L):
         L2 = []
         for e in L :
             L2.append(e)
-            L2.append(e[0])
-            L2.append(-e[1])
-            L2.append(-e[0])
-            L2.append(e[1])
-            L2.append(-e[0])
-            L2.append(-e[1])
+            L2.append((e[0],-e[1]))
+            L2.append((-e[0],e[1]))
+            L2.append((-e[0],-e[1]))
         return L2
 
-def verif_case(coup,piece):
+def verif_case(coup,piece): #coup = (x2,y2)
         pos = piece[2]
-        if not (coup[0] >= 0 and coup[0] <= 7 and coup[1] <=7 and coup[0]>=0) :
+        if not board(coup) :
             return 0
         
-        if Chessboard.cases[(coup[0],coup[1])][0] == 0 :
+        if Chessboard.cases[coup][0] == 0 :
             return 1
         else :
-            if Chessboard.cases[(coup[0],coup[1])][2] == piece[0]:
+            if Chessboard.cases[coup][2] == piece[0]:
                 return 2
             
+def board((x,y)):
+    if x<=7 and y<=7 and x>=0 and y>=0 :
+        return True
+    return False
 
 def possibles(moi,x1,y1):
         #si on a le temps on fera s'afficher la liste des cases possibles
         poss = {}
-
-        piece = [moi[1],moi[2],[x1,y1]]
-        pos = [x1,y1]
+        piece = [moi[1],moi[2],(x1,y1)]
 
         if piece[0] == 'c' or piece[0] == 'r':
-            for move in rev(moves[piece[0]]) :
-                pos2 = np.array([pos]) + np.array([move])
+            for coup in rev(moves[piece[0]]) :
+                pos2 = np.array([piece[2]]) + np.array([coup])
                 if verif_case(pos2,piece()) >= 1 :
                     poss[pos2] = 0
         
         else :
             for dir in rev(moves[piece[0]]):
-                while np.array(pos2) != (np.array([pos]) + np.array([move])):
+                pos2 = piece[2]
+                while board(pos2+dir) :
                     pos2 += dir
-                    if verif_case(pos2,piece()) >= 1 :
+                    if verif_case(pos2,piece) >= 1 :
                         poss[pos2] = 0
-                    if verif_case(pos2,piece()) == 2 :
+                    if verif_case(pos2,piece) == 2 :
                         continue
         
+        if piece[0] == 'p':
+             
+
+
+
+
+
+
+
+
+
+
         return poss
 
 
@@ -66,21 +77,20 @@ def coup_valide(self):
             return False
         
 
-'''
-def en_echec (couleur):
+
+
+
+def en_echec (couleur):    #couleur c'est 1 ou 0 selon la couelur qui est peutetre en échec
     for x in range(7):
         for y in range(7):
             essai = Chessboard.cases[(x,y)]
 
             if essai[1] == couleur + 1 or essai[1] == couleur - 1 :
-                if roi in possibles(essai,x,y):'''
-                     
-                     
-
-
-
-
-
+                for pos in possibles(essai,x,y):
+                    if Chessboard.cases[pos][1] == 'r':
+                        return True
+    
+    return False
 
 
 
